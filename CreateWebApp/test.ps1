28f851bb05a745ac38e6be9ea27d112e8e1827dd
Elie Elmalem <elielmal@WKMZTE164E7D.global.publicisgroupe.net>
2021-07-16 11:43:20 +0200

    winrm tests

---
+++ CreateWebApp/test.ps1
@@ -0,0 +1,25 @@
+$certificateName = "certificatetest"
+
+$thumbprint = (New-SelfSignedCertificate -DnsName $certificateName -CertStoreLocation Cert:\CurrentUser\My -KeySpec KeyExchange).Thumbprint
+
+$cert = (Get-ChildItem -Path cert:\CurrentUser\My\$thumbprint)
+
+$password = Read-Host -Prompt "Please enter the certificate password." -AsSecureString
+
+Export-PfxCertificate -Cert $cert -FilePath ".\$certificateName.pfx" -Password $password
+
+
+$fileName = ".\"
+$fileContentBytes = Get-Content $fileName -Encoding Byte
+$fileContentEncoded = [System.Convert]::ToBase64String($fileContentBytes)
+
+[System.Collections.HashTable]$TableForJSON = @{
+    "data"     = $fileContentEncoded;
+    "dataType" = "pfx";
+    "password" = "Thisisthepass123";
+}
+[System.String]$jsonObject = $TableForJSON | ConvertTo-Json 
+$jsonEncoded = [System.Convert]::ToBase64String($jsonObject)
+
+$secret = ConvertTo-SecureString -String $jsonEncoded -AsPlainText –Force
+Set-AzKeyVaultSecret -VaultName "<vault name>" -Name "<secret name>" -SecretValue $secret
